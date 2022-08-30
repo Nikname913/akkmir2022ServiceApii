a9a00327de5b2a7f58db85ed35d3dccd7e44d403
nik shipov <nik.shipov@gmail.com>
2022-08-30 10:21:21 +0500

    UPD: update 300822

--- akkmirApi/urls.py
+++ akkmirApi/urls.py
@@ -8,14 +8,28 @@ urlpatterns = [
     # ----------------------------
     path('example', views.pingMethod, name='EXAMPLE'),
     # ----------------------------
-    # этот и далее методы работают через POST и GET запросы
     # метод записи данных их xml файла в базовый json файл
     # ----------------------------
     path('writeXML', views.writeXmlData, name='WRITE_XML'),
+    # ----------------------------
+    # получить список сервис-центров и складов
+    # ----------------------------
     path('getServicesList', views.getServicesList, name='GET_SERVICES'),
+    # ----------------------------
+    # получить список доступных услуг
+    # ----------------------------
     path('getUslugiList', views.getUslugiList, name='GET_USLUGI'),
+    # ----------------------------
+    # получить список всех записей в сц
+    # ----------------------------
     path('getWritingsList', views.getWritingsList, name='GET_WRITINGS'),
+    # ----------------------------
+    # получить информацию об одном сервис центре
+    # ----------------------------
     path('getServicesListOne', views.getServicesListOne, name='GET_SERVICE_ONE'),
+    # ----------------------------
+    # добавить новую запись в один из сервис-центров
+    # ----------------------------
     path('newWriting', views.newWriting, name='NEW_WRITING'),
     
 ]
